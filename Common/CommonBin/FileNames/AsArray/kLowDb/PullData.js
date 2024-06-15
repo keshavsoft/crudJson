a31b2e7ab2acc1eefd20f0aa83bd1eed8dd857ac
Keshav <nknnkumar@yahoo.com>
2024-06-15 19:17:53 +0530

    keshav

--- Common/CommonBin/FileNames/AsArray/kLowDb/PullData.js
+++ Common/CommonBin/FileNames/AsArray/kLowDb/PullData.js
@@ -5,11 +5,30 @@ import ConfigJson from '../../../../../bin/Config.json' assert {type: 'json'};
 
 let StartFunc = () => {
     let LocalDataPath = `${ConfigJson.jsonConfig.DataPath}/${ConfigJson.jsonConfig.DataPk}`;
+    // const tree = dirTree(LocalDataPath);
+    let LocalMap = FuncForJsonFilesInFolder({ inRootFolder: LocalDataPath });
+
+    return LocalMap;
+};
+
+let StartFunc_15Jun2024 = () => {
+    let LocalDataPath = `${ConfigJson.jsonConfig.DataPath}/${ConfigJson.jsonConfig.DataPk}`;
     const tree = dirTree(LocalDataPath);
     let LocalMap = tree.children.map(e => path.parse(e.name).name);
 
-
     return LocalMap;
+};
+
+const FuncForJsonFilesInFolder = ({ inRootFolder }) => {
+    const root = inRootFolder;
+    let files = {}
+    let LocalFilesAray = [];
+    let k1 = fs.readdirSync(root);
+
+    LocalFilesAray = fs.readdirSync(root)
+        .filter(filename => filename.endsWith('.json'));
+
+    return LocalFilesAray;
 };
 
 export { StartFunc };
