f277bd2b31e2d9b34de87d70d74332f11198541a
keshavsoft <nknnkumar@yahoo.com>
2024-06-19 21:57:31 +0530

    keshav

--- src/FrontEnd/assets/static/js/pages/Create/FormLoad/AddListeners/SaveButtonId/FetchFromFuncs/Entry.js
+++ src/FrontEnd/assets/static/js/pages/Create/FormLoad/AddListeners/SaveButtonId/FetchFromFuncs/Entry.js
@@ -1,13 +1,15 @@
 import { StartFunc as StartFuncFetchFuncs } from "./PostFetch.js";
-
+import { StartFunc as CheckFunc } from "./CheckFunc.js";
 import { StartFunc as StartFuncAfterFetch } from "./AfterFetch/EntryFile.js";
 
 let StartFunc = async () => {
-    let jVarLocalDataNeeded = await StartFuncFetchFuncs();
+    if (CheckFunc()) {
+        let jVarLocalDataNeeded = await StartFuncFetchFuncs();
 
-    if (jVarLocalDataNeeded !== null) {
-        if (jVarLocalDataNeeded) {
-            StartFuncAfterFetch();
+        if (jVarLocalDataNeeded !== null) {
+            if (jVarLocalDataNeeded) {
+                StartFuncAfterFetch();
+            };
         };
     };
 };
