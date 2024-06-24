35cca0b368bc2a00fb9ff7cef3b5d57831b31e4c
Keshav Nalam <nknnkumar@gmail.com>
2024-06-24 09:36:48 +0530

    keshav

--- src/FrontEnd/assets/static/js/pages/CreateClientSide/FormLoad/AddListeners/SaveButtonId/FetchFromFuncs/AfterFetch/EntryFile.js
+++ src/FrontEnd/assets/static/js/pages/CreateClientSide/FormLoad/AddListeners/SaveButtonId/FetchFromFuncs/AfterFetch/EntryFile.js
@@ -1,12 +1,7 @@
-let StartFunc = () => {
-    // const url = new URL(window.location.href);
+import UrlJson from './url.json' with {type: 'json'};
 
-    window.location.href = "ShowSimple.html";
-    // console.log("aaaaaaaaaa : ", k1);
-    // const params1 = new URLSearchParams(url.search);
-    // let NewURl = new URL("../ShowAll/ShowAll.html", url);
-    // const new_url = new URL(`${NewURl.href}?${params1}`);
-    // window.location.href = new_url;
+let StartFunc = () => {
+    window.location.href = `${jVarGlobalTableName}${UrlJson.RedirectToUrl}`;
 };
 
 export { StartFunc }
