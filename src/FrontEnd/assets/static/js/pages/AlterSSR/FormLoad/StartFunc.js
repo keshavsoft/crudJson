f644158fa02e24e3a3bd2d08fc9defe63bff3ca8
Keshav Nalam <nknnkumar@gmail.com>
2024-06-27 23:42:55 +0530

    keshav

--- src/FrontEnd/assets/static/js/pages/AlterSSR/FormLoad/StartFunc.js
+++ src/FrontEnd/assets/static/js/pages/AlterSSR/FormLoad/StartFunc.js
@@ -1,10 +1,8 @@
 import { StartFunc as ShowOnDom } from "./ShowOnDom/EntryFile.js";
-import { StartFunc as BuildSubTable } from "./BuildSubTable/EntryFile.js";
 import { StartFunc as StartFuncAddListeners } from "./AddListeners/StartFunc.js";
 
-const StartFunc = async () => {
+const StartFunc = () => {
     ShowOnDom().then();
-    BuildSubTable().then();
     StartFuncAddListeners();
 };
 
