0ce8b3cd9f27334d3dd19610cb90a6e4d34c7c36
Srivasu215 <sri.vasu215@gmail.com>
2024-06-27 12:20:01 +0530

    SREENIVAS

--- src/FrontEnd/assets/static/js/Common/NiceAdmin/Header/HeaderMastersId/FetchFromFuncs/PostFetch.js
+++ src/FrontEnd/assets/static/js/Common/NiceAdmin/Header/HeaderMastersId/FetchFromFuncs/PostFetch.js
@@ -1,13 +1,13 @@
-import { StartFunc as StartFuncFetchHeaders } from "./FetchHeaders/EntryFile.js";
+// import { StartFunc as StartFuncFetchHeaders } from "./FetchHeaders/EntryFile.js";
 import UrlJson from "./url.json" assert { type: "json" };
 
 let StartFunc = async () => {
     let LocalroutePath = UrlJson.PostUrl;
     let LocaltableName = jVarGlobalTableName;
 
-    let jVarLocalFetchHeaders = StartFuncFetchHeaders();
-    let jVarLocalFetchUrl = `/${LocalroutePath}/${LocaltableName}`;
-    let response = await fetch(jVarLocalFetchUrl, jVarLocalFetchHeaders);
+    // let jVarLocalFetchHeaders = StartFuncFetchHeaders();
+    let jVarLocalFetchUrl = `/${LocalroutePath}/${LocaltableName}/DataOnly`;
+    let response = await fetch(jVarLocalFetchUrl);
     let data = await response.json();
 
     return await data;
