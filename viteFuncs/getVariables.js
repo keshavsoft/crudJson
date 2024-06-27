820f68e1f2907da772c6f78dbb1841c05f6652a6
Keshav Nalam <nknnkumar@gmail.com>
2024-06-27 09:55:22 +0530

    keshav

--- viteFuncs/getVariables.js
+++ viteFuncs/getVariables.js
@@ -1,6 +1,5 @@
 import ConfigJson from '../bin/Config.json' with {type: 'json'};
 import { StartFunc as ReadDataSchema } from "./ReadDataSchema.js";
-import { StartFunc as ReadTableSchema } from "./ReadTableSchema.js";
 import path from "path";
 
 const StartFunc = ({ mode, inFilesArray, inSidebarItems }) => {
@@ -9,9 +8,7 @@ const StartFunc = ({ mode, inFilesArray, inSidebarItems }) => {
     let sidebarItems = inSidebarItems;
     let LocalFilteredSideBarItems = LocalFuncFilterSideBarItems({ inSidebarItems: sidebarItems });
 
-    let jVarLocalTableSchema = LocalFuncGetTableData();
-    // console.log("jVarLocalTableSchema ", jVarLocalTableSchema, LocalFilteredSideBarItems);
-    let LoopInsidecolumnData = LocalFuncGetColumnData();
+    // let LoopInsidecolumnData = LocalFuncGetColumnData();
 
     Object.keys(LocalFiles).forEach((filename) => {
         if (filename.includes('layouts/FrontEnd')) filename = `layouts/FrontEnd/${filename}`
@@ -21,8 +18,9 @@ const StartFunc = ({ mode, inFilesArray, inSidebarItems }) => {
         });
 
         if (LoopInsideFindSideBar === undefined === false) {
+            let LoopInsidecolumnData = LocalFuncGetColumnData({ inTableName: filename });
 
-            // console.log("filename ", filename, LoopInsideFindSideBar);
+            console.log("LoopInsidecolumnData ", filename, LoopInsidecolumnData);
             variables[filename + '.html'] = {
                 web_title: "Mazer Admin Dashboard",
                 filename: filename + '.html',
@@ -38,42 +36,6 @@ const StartFunc = ({ mode, inFilesArray, inSidebarItems }) => {
     return variables;
 };
 
-const StartFunc_Keshav_22Jun2024 = ({ mode, inFilesArray, inSidebarItems }) => {
-    const variables = {};
-    let LocalFiles = inFilesArray;
-    let sidebarItems = inSidebarItems;
-    let TableSchema = ReadDataSchema();
-    let jVarLocalTableSchema = ReadTableSchema();
-    console.log("jVarLocalTableSchema : ", jVarLocalTableSchema.children);
-
-    let LoopinsideFind = TableSchema.find(element => {
-        return "Customers".startsWith(path.parse(element.name).name);
-    });
-
-    let LoopInsidecolumnData = {};
-
-    if (LoopinsideFind === undefined === false) {
-        LoopInsidecolumnData = LoopinsideFind.fileData;
-    };
-
-    console.log("TableSchema : ", TableSchema);
-    Object.keys(LocalFiles).forEach((filename) => {
-        if (filename.includes('layouts/FrontEnd')) filename = `layouts/FrontEnd/${filename}`
-
-        variables[filename + '.html'] = {
-            web_title: "Mazer Admin Dashboard",
-            filename: filename + '.html',
-            sidebarItems,
-            isDev: mode === 'development',
-            DataPk: ConfigJson.jsonConfig.DataPk,
-            tableName: "Customers",
-            columnData: LoopInsidecolumnData
-        }
-    });
-
-    return variables;
-};
-
 const LocalFuncFilterSideBarItems = ({ inSidebarItems }) => {
     let LocalReturnArray;
 
@@ -84,28 +46,11 @@ const LocalFuncFilterSideBarItems = ({ inSidebarItems }) => {
     return LocalReturnArray;
 };
 
-const LocalFuncGetColumnData = () => {
+const LocalFuncGetColumnData = ({ inTableName }) => {
     let TableSchema = ReadDataSchema();
 
     let LoopinsideFind = TableSchema.find(element => {
-        return "Customers".startsWith(path.parse(element.name).name);
-    });
-
-    let LoopInsidecolumnData = {};
-
-    if (LoopinsideFind === undefined === false) {
-        LoopInsidecolumnData = LoopinsideFind.fileData;
-    };
-
-    return LoopInsidecolumnData;
-};
-
-
-const LocalFuncGetTableData = () => {
-    let TableSchema = ReadTableSchema();
-
-    let LoopinsideFind = TableSchema.find(element => {
-        return "Customers".startsWith(path.parse(element.name).name);
+        return inTableName.startsWith(path.parse(element.name).name);
     });
 
     let LoopInsidecolumnData = {};
