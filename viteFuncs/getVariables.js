543d6dbaae315ef83b863e00acf2b323eba05255
keshavsoft <nknnkumar@yahoo.com>
2024-06-28 22:33:23 +0530

    keshav

--- viteFuncs/getVariables.js
+++ viteFuncs/getVariables.js
@@ -1,75 +1,14 @@
-import ConfigJson from '../bin/Config.json' with {type: 'json'};
-import sideBarSingleTable from '../KCode/ForFrontEndSingleTable/sideBarSingleTable.json' with {type: 'json'};
-import sideBarItemsAllTables from '../KCode/ForFrontEndSingleTable/sideBarItemsAllTables.json' with {type: 'json'};
-
-import { StartFunc as mainTableSchema } from "./generateVariables/mainTableSchema.js";
-import { StartFunc as mainTableColumnsConfig } from "./generateVariables/mainTableColumnsConfig.js";
-import { StartFunc as foreignTableColumnsConfig } from "./generateVariables/foreignTableColumnsConfig.js";
-
-import path from "path";
-import _ from "lodash";
+import { StartFunc as AllTables } from "./generateVariables/buildType/AllTables.js";
+import { StartFunc as FirstTable } from "./generateVariables/buildType/FirstTable.js";
 
 const StartFunc = ({ mode, inFilesArray, inBuildType }) => {
-    const variables = {};
-    let LocalFiles = inFilesArray;
-    let sidebarItems = LocalFuncForSideBar({ inBuildType });
-    let LocalFilteredSideBarItems = LocalFuncFilterSideBarItems({ inSidebarItems: sidebarItems });
-
-    Object.keys(LocalFiles).forEach((filename) => {
-        if (filename.includes('layouts/FrontEnd')) filename = `layouts/FrontEnd/${filename}`
-
-        let LoopInsideFindSideBar = LocalFilteredSideBarItems.find(element => {
-            return filename.startsWith(element.name);
-        });
-
-        if (LoopInsideFindSideBar === undefined === false) {
-            let LoopInsidecolumnData = mainTableColumnsConfig({ inTableName: filename });
-            let LoopInsideTableConfig = mainTableSchema({ inTableName: filename });
-
-            let LocalInsideForeignTable = foreignTableColumnsConfig({ inTableName: LoopInsideFindSideBar.name });
-            let LocalInsideSubTableName = "";
-
-            if (LocalInsideForeignTable === undefined === false) {
-                LocalInsideSubTableName = path.parse(LocalInsideForeignTable?.name)?.name;
-            };
-
-            // console.log("- ", filename, LocalInsideForeignTable);
-            variables[filename + '.html'] = {
-                web_title: "Mazer Admin Dashboard",
-                filename: filename + '.html',
-                sidebarItems,
-                isDev: mode === 'development',
-                DataPk: ConfigJson.jsonConfig.DataPk,
-                tableName: LoopInsideFindSideBar.name,
-                subTableName: LocalInsideSubTableName,
-                columnData: LoopInsidecolumnData,
-                tableConfig: LoopInsideTableConfig,
-                foreignTablecolumnData: LocalInsideForeignTable?.fileData
-            };
-        };
-    });
-
-    return variables;
-};
-
-const LocalFuncForSideBar = ({ inBuildType }) => {
     if (inBuildType === "FirstTable") {
-        return sideBarSingleTable;
+        return FirstTable({ mode, inFilesArray, inBuildType });
     };
 
     if (inBuildType === "AllTables") {
-        return sideBarItemsAllTables;
+        return AllTables({ mode, inFilesArray, inBuildType });
     };
-};
-
-const LocalFuncFilterSideBarItems = ({ inSidebarItems }) => {
-    let LocalReturnArray;
-
-    LocalReturnArray = inSidebarItems.filter(element => {
-        return "children" in element;
-    });
-
-    return LocalReturnArray;
 };
 
 export { StartFunc };
