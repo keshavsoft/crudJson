399a168a3dcabe10aab6a987828f9bad72b4597d
Srivasu215 <sri.vasu215@gmail.com>
2024-07-01 12:26:13 +0530

    SREENIVAS

--- src/FrontEnd/assets/static/js/pages/BulkInsert/FormLoad/StartFunc.js
+++ src/FrontEnd/assets/static/js/pages/BulkInsert/FormLoad/StartFunc.js
@@ -1,7 +1,9 @@
 import { StartFunc as StartFuncAddListeners } from "./AddListeners/StartFunc.js";
 import { StartFunc as StartFuncBuildBsTable } from "./BuildBsTable/EntryFile.js";
+import { StartFunc as StartFuncShowAlerts } from "./ShowAlerts.js";
 
 const StartFunc = () => {
+    StartFuncShowAlerts();
     StartFuncBuildBsTable();
     StartFuncAddListeners();
 };
